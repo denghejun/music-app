6094c07f6798e6861bee659eb2afed14e4f722d5
denghejun <denghejun@live.cn>
2016-01-31 19:24:58 +0800

    commit : mvvm light demao

---
+++ UsingMVVMLightDemo/UsingMVVMLightDemo/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UsingMVVMLightDemo
+{
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand MouseDownCommand
+        {
+            get
+            {
+                return new RelayCommand<object>(e =>
+                {
+
+                });
+            }
+        }
+    }
+}
